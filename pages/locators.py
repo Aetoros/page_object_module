5bbe13ea7d397f058f8a8e3fa83cfff1c96cf910
Alexander <Aetoros@ya.ru>
2021-07-08 21:37:42 +0500

    adding parametrization

---
+++ pages/locators.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class BasketLocator():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, "#add_to_basket_form button.btn")
+    CHECK_TITLE = (By.CSS_SELECTOR, ".col-sm-6 h1")
+    CHECK_TITLE_AFTER_ADDING = (By.CSS_SELECTOR,".alert:nth-child(1) strong")
+     
+ 
--- test_main_page.py
+++ test_main_page.py
@@ -1,16 +1,25 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+import pytest
+import time
 
-
-
+@pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()
     page.should_be_login_link()
 
+    page.go_to_login_page()
+    login = LoginPage(browser, browser.current_url)
+    login.should_be_login_page()
 
-    page = LoginPage(browser, link)
-    page.should_be_login_page()   
-
+@pytest.mark.parametrize('different_links', ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])         
+def test_add_to_basket(browser, different_links):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{different_links}"
+    page = MainPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.check_title()
+    
