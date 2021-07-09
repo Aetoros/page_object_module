98c0d14a90b6efa5046a3635c55c4cbd4296e074
Alexander <Aetoros@ya.ru>
2021-07-09 12:10:33 +0500

    finalazing and cleaning up

--- test_product_page.py
+++ test_product_page.py
@@ -12,7 +12,24 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.open()
     page.guest_cant_see_product_in_basket_opened_from_product_page()
 
-@pytest.mark.login
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page (browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+@pytest.mark.need_review
+@pytest.mark.parametrize('different_links', ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])         
+def test_guest_can_add_product_to_basket(browser, different_links):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{different_links}"
+    page = BasketPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.check_title()
+
+@pytest.mark.need_review
 class TestUserAddToBasketFromProductPage():
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
@@ -25,12 +42,14 @@ class TestUserAddToBasketFromProductPage():
         page.should_be_authorized_user()
     def test_user_cant_see_product_in_basket_opened_from_product_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
-        page = BasketPage(browser, link, 4)
+        page = BasketPage(browser, link)
         page.open()
         page.guest_cant_see_product_in_basket_opened_from_product_page()
 
-    def test_user_cant_see_product_in_basket_opened_from_main_page(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
-        page = BasketPage(browser, link, 4)
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+        page = BasketPage(browser, link)
         page.open()
-        page.guest_cant_see_product_in_basket_opened_from_main_page()
+        page.add_to_basket()
+        page.solve_quiz_and_get_code()
+        page.check_title()
